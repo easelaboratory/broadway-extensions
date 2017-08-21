7eada857f47bc465648eb1893173a1e2ff71aced
Francesco Trucchia <ft@ideato.it>
2017-08-21 02:39:37 +0200

    Add command builder

---
+++ src/BroadwayExtensions/CommandHandling/CommandBuilder.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the easelaboratory/broadway-extensions package.
+ *
+ * (c) easelab.it <os@easelab.it>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BroadwayExtensions\CommandHandling;
+
+/**
+ * Class CommandBuilder
+ * @package Soisy\Cqrs\CommandHandling
+ */
+class CommandBuilder
+{
+    protected $classesMap = [];
+
+    /**
+     * CommandBuilder constructor.
+     *
+     * @param array $classesMap
+     */
+    public function __construct(array $classesMap = [])
+    {
+        $this->classesMap = $classesMap;
+    }
+
+    /**
+     * @param array $classeMap
+     */
+    public function setClassesMap(array $classeMap)
+    {
+        $this->classesMap = $classeMap;
+    }
+
+    /**
+     * @param $className
+     * @param $params
+     * @return object
+     */
+    public function build($className, $params)
+    {
+        $class      = new \ReflectionClass($className);
+        $parameters = $class->getConstructor()->getParameters();
+
+        $args = [];
+        foreach ($params as $index => $param) {
+            if (!isset($parameters[$index])) {
+                throw new \RangeException('Invalid parameters with offset ' . $index);
+            }
+            if ($parameters[$index]->getClass()) {
+                $className = $parameters[$index]->getClass()->getName();
+                if (isset($this->classesMap[$className])) {
+                    $className = $this->classesMap[$className];
+                }
+
+                if (is_callable($className)) {
+                    $args[] = $className($param);
+                } else {
+                    $args[] = new $className($param);
+                }
+            } else {
+                $args[] = $param;
+            }
+        }
+
+        return $class->newInstanceArgs($args);
+    }
+}
---
+++ test/BroadwayExtensions/CommandHandling/CommandBuilderTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the easelaboratory/broadway-extensions package.
+ *
+ * (c) easelab.it <os@easelab.it>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BroadwayExtensions\CommandHandling;
+
+use BroadwayExtensions\TestCase;
+use Webmozart\Assert\Assert;
+
+class CommandBuilderTest extends TestCase
+{
+    private $classesMap;
+
+    public function setUp()
+    {
+        $this->classesMap = [
+            \DateTimeInterface::class => \DateTimeImmutable::class,
+            StaticName::class => function ($value) {
+                return StaticName::create((int)$value);
+            }
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function map()
+    {
+        $params[] = '1';
+        $params[] = 'Francesco';
+        $params[] = '2017-01-01';
+
+        $builder = new CommandBuilder();
+        $builder->setClassesMap($this->classesMap);
+        $command = $builder->build(TestCommand::class, $params);
+
+        $this->assertInstanceOf(TestCommand::class, $command);
+    }
+
+    /**
+     * @test
+     * @expectedException \RangeException
+     */
+    public function invalidParamsRange()
+    {
+        $params[] = '1';
+        $params[] = 'Francesco';
+        $params[] = '2017-01-01';
+        $params[] = '';
+
+        $builder = new CommandBuilder();
+        $builder->setClassesMap($this->classesMap);
+        $builder->build(TestCommand::class, $params);
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function invalidParamValue()
+    {
+        $params[] = 'a';
+        $params[] = 'Francesco';
+        $params[] = '2017-01-01';
+
+        $builder = new CommandBuilder();
+        $builder->setClassesMap($this->classesMap);
+        $builder->build(TestCommand::class, $params);
+    }
+}
+
+class ValueObjectId
+{
+    private $id;
+
+    public function __construct($id)
+    {
+        Assert::numeric($id);
+        $this->id = $id;
+    }
+}
+
+class StaticName
+{
+    private $value;
+
+    public static function create($value)
+    {
+        return new self($value);
+    }
+
+    private function __construct($value)
+    {
+        $this->value = $value;
+    }
+}
+
+class TestCommand
+{
+    private $id;
+    private $name;
+    private $date;
+
+    public function __construct(ValueObjectId $id, StaticName $name, \DateTimeInterface $date)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->date = $date;
+    }
+}
